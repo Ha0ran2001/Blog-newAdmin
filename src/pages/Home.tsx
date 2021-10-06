2166724c53e65d690c343945fd5b379549b9735e
Rookoe-haoran <bababylon88@gmail.com>
2021-10-06 20:42:22 +0800

    style: width>md时的滚轴取消

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,7 +40,7 @@ const Home: React.FC<HomeProps> = () => {
 
   return (
     <div className='dark:bg-gray-900 text-white box-border relative'>
-      <main className='w-full border-2 border-purple-400 dark:border-blue-900 rounded-md relative md:h-[80vh] xl:h-[76vh] flex flex-col md:flex-row'>
+      <main className='w-full border-2 border-purple-400 dark:border-blue-900 rounded-md relative md:h-[76vh] flex flex-col md:flex-row'>
         <div className='flex md:flex-col overflow-x-scroll overflow-y-hidden  md:w-[400px] md:overflow-y-scroll md:overflow-x-hidden'>
           <div className='w-[100px] md:w-full md:h-[60px] h-36 inline-block whitespace-nowrap align-middle p-5'>
             <div className='w-full h-full cursor-pointer text-yellow-300' onClick={() => history.push('/new')}>添加</div>
